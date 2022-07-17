f8df4a79bb29ce476774a12fdc6354e1097960b4
Sanizzang <tksl4732@naver.com>
2022-07-17 14:19:49 +0900

    add core

---
+++ core/src/test/java/hello/core/singleton/SingletonTest.java
@@ -0,0 +1,21 @@
+package hello.core.singleton;
+
+import hello.core.AppConfig;
+import hello.core.memeber.MemberService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class SingletonTest {
+    @Test
+    @DisplayName("스프링 없는 순수한 DI 컨테이너")
+    void pureContainer() {
+        AppConfig appConfig = new AppConfig();
+        //1. 조회: 호출할 때 마다 객체를 생성
+        MemberService memberService1 = appConfig.memberService();
+
+        //2. 조회: 호출할 때 마다 객체를 생성
+        MemberService memberService2 = appConfig.memberService();
+
+        //참조값이 다른 것을 확인인
+    }
+}
